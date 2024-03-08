3276eddcaa3ba1f36da55d5a10141ad4f98a8bba
Charan <charan.adhish@gmail.com>
2024-03-08 15:59:16 -0600

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -48,8 +48,8 @@ export default function Home() {
     event.preventDefault(); // Prevent default form submission behavior
     // Construct the URL with query parameters
     const queryParams = new URLSearchParams(formData).toString();
-    // const url = `https://txbus-production.up.railway.app/fetchBusData?${queryParams}`; // Ensure this URL matches your Flask endpoint and port
-    const url = `http://127.0.0.1:5000/fetchBusData?${queryParams}`; // Ensure this URL matches your Flask endpoint and port
+    const url = `https://txbus-production.up.railway.app/fetchBusData?${queryParams}`; // Ensure this URL matches your Flask endpoint and port
+    // const url = `http://127.0.0.1:5000/fetchBusData?${queryParams}`; // Ensure this URL matches your Flask endpoint and port
 
     try {
       const response = await fetch(url); // GET request
@@ -147,37 +147,33 @@ export default function Home() {
         </form>
 
         {Array.isArray(searchResults) && searchResults.length > 0 && (
-          <div className="mt-8">
-            <h2 className="text-2xl font-bold mb-4">Search Results</h2>
+          <div className="mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <h2 className="text-2xl font-bold mb-4 col-span-full">
+              Search Results
+            </h2>
             {searchResults.map((result, index) => (
               <div
                 key={index}
-                className="p-4 mb-4 border-2 border-gray-200 rounded-lg"
+                className="p-4 mt-2 border-2 border-gray-200 rounded-lg"
               >
-                <h2 className="text-xl font-semibold">{result.operator}</h2>
-                <p className="font-medium">
-                  Date: <span className="font-normal">{result.date}</span>
-                </p>
-                <p className="font-medium">
-                  Departure:{" "}
-                  <span className="font-normal">
-                    {result.departureCity} ({result.departureStation}) at{" "}
-                    {result.departureTime}
-                  </span>
-                </p>
-                <p className="font-medium">
-                  Arrival:{" "}
-                  <span className="font-normal">
-                    {result.arrivalCity} ({result.arrivalStation}) at{" "}
-                    {result.arrivalTime}
-                  </span>
-                </p>
-                <p className="font-medium">
-                  Price:{" "}
-                  <span className="font-normal">
+                <div className="flex justify-between items-center mb-2">
+                  <h3 className="text-xl font-semibold">{result.operator}</h3>
+                  <span className="text-sm bg-blue-200 text-blue-800 rounded-full px-2 py-1">
                     ${result.price.toFixed(2)}
                   </span>
-                </p>
+                </div>
+                <div className="flex justify-between items-center mb-1">
+                  <span className="font-normal">{result.departureTime}</span>
+                  <span className="font-normal">{result.arrivalTime}</span>
+                </div>
+                <div className="flex justify-between items-center">
+                  <span className="truncate max-w-sm font-light">
+                    {result.departureStation}
+                  </span>
+                  <span className="truncate max-w-sm font-light">
+                    {result.arrivalStation}
+                  </span>
+                </div>
               </div>
             ))}
           </div>
